b3b961942e976ebce1ef77924f435b7811a972ed
Missmiesharh <ayshamustapha99@gmail.com>
2023-01-18 09:47:32 +0100

    initial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/navbar/navbar';
+import Home from './components/Home/home'
+
+function App() {
+  return (
+   <>
+    <Navbar />
+    <Home />
+    
+   </>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <>
+        <h2>Home</h2>
+        <h2>All Movies</h2>
+        <h2>Create Movies</h2>
+        <
+
+    </>
+  )
+}
+
+export default navbar
